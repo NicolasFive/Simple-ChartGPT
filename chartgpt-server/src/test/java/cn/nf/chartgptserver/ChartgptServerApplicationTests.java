29acb82b8a93311c77bfa30eb223477196ba0972
wujin <wu.jin@ustcinfo.com>
2023-05-21 12:51:32 +0800

    Upload code

--- chartgpt-server/src/test/java/cn/nf/chartgptserver/ChartgptServerApplicationTests.java
+++ chartgpt-server/src/test/java/cn/nf/chartgptserver/ChartgptServerApplicationTests.java
@@ -4,7 +4,4 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 class ChartgptServerApplicationTests {
 
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("Admin@2023"));
-    }
 }
